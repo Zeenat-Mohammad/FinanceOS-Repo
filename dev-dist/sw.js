080f4feeb83b141728df809b31581b86bd7efb0d
Hajra Mohammad Shahid <hajra.mshahid24@gmail.com>
2026-07-19 22:30:12 +0530

    Updated Landing page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-290dd570'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h4lalabljmg"
+    "revision": "0.sas6sv00dp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
